eafad3c07b5acec1bb8784524ffb81c300aab51a
Châu Khắc Tuấn Anh <tuananhchaukhac@Chaus-MacBook-Pro.local>
2024-12-30 13:50:49 +0700

    Fix table.tsx

--- components/table/table.tsx
+++ components/table/table.tsx
@@ -1,42 +1,36 @@
-import React from 'react'
+import React from 'react';
 
-interface TableProps{
-    columns: {header:string; key:string; className?:string}[];
-    renderRow:(item:any)=>React.ReactNode;
-    data:any[];
+interface TableProps {
+  columns: { header: string; key: string; className?: string }[];
+  renderRow: (item: any) => React.ReactNode;
+  data: any[];
 }
 
-export const Table = ({columns,renderRow,data}:TableProps) => {
-    return  <table className ="w-full mt-4">
+export const Table = ({ columns, renderRow, data }: TableProps) => {
+  return (
+    <table className="w-full mt-4">
       <thead>
         <tr className="text-left text-gray-500 text-sm lg:uppercase">
-  {
-    columns.map(({header, key,className})=>(
-      <th key={key} className={className}>
-        {header}
-      </th>
-    ))
-  }
+          {columns.map(({ header, key, className }) => (
+            <th key={key} className={className}>
+              {header}
+            </th>
+          ))}
         </tr>
       </thead>
-      <tbody>{
-      data?.length < 1 && 
-      (<tr className="text-gray-400 text-base py-10"> No Data Found
-      </tr>)
-      }
-      {
-      data?.length > 0 && (
-        data?.map((item,id)=>(
-          renderRow({...item,index:id})
-  
-        )
-      )
-    )
-  }
-      
-  
+      <tbody>
+        {data?.length < 1 && (
+          <tr>
+            <td colSpan={columns.length} className="text-center text-gray-400 text-base py-10">
+              No Data Found
+            </td>
+          </tr>
+        )}
+        {data?.length > 0 &&
+          data.map((item, id) => (
+            <React.Fragment key={id}>{renderRow({ ...item, index: id })}</React.Fragment>
+          ))}
       </tbody>
-      
     </table>
-    
-  }
+  );
+};
